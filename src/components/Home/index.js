12000c35114484e793768544fd7a49dfda424f29
sireesha <msireesha30031999@gmail.com>
2022-03-11 21:51:09 +0530

    HomeItem

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,69 @@
 import {Component} from 'react'
 import './index.css'
+import TodoList  from'../TodoList'
+
+const todoList =[
+    {
+        id:1,
+        title:'learning frontend'
+    },
+    {
+        id:2,
+        title:'learning backend'
+    }
+    ,{
+        id:3,
+        title:'Watching Movie'
+    },{
+        id:4,
+        title:'learning frontend'
+    },
+    {
+        id:5,
+        title:'learning backend'
+    }
+    ,{
+        id:6,
+        title:'Watching Movie'
+    },{
+        id:7,
+        title:'learning frontend'
+    },
+    {
+        id:8,
+        title:'learning backend'
+    }
+    ,{
+        id:9,
+        title:'Watching Movie'
+    },{
+        id:10,
+        title:'learning backend'
+    }
+    ,{
+        id:11,
+        title:'Watching Movie'
+    }
+]
 
 class Home extends Component{
     state = {userSearchInput:""}
 
-    onClickOfAddTask =event=>{
-        const taskDescription = {title:'Learning BootStrap'};
+    onChangeOfInputEl = event =>{
+        console.log(event.target.value);
+        this.setState({userSearchInput:event.target.value})
+    }
+    onClickOfAddTask = () => {
+        const taskDescription = {title:this.state.userSearchInput};
         console.log(taskDescription)
         const url = "https://todolist-django-backend.herokuapp.com/todo/";
         let options = {
-            method: 'GET',
+            method: 'POST',
             headers: {
                 'Content-Type': 
                     'application/json;charset=utf-8'
             },
-            // body: JSON.stringify(taskDescription)
+            body: JSON.stringify(taskDescription)
         }
 
         let fetchRes = fetch(url, options);
@@ -31,10 +80,11 @@ class Home extends Component{
 
                     <label htmlFor='todo-input' className='label-el'>Create Task</label>
                     <div className='btn-input-container'>
-                        <input className='input-el' id='todo-input' type="search"/>
+                        <input className='input-el' id='todo-input' type="text" onChange={this.onChangeOfInputEl}/>
                         <button className='task-add-btn' onClick={this.onClickOfAddTask}>Add Task</button>
                     </div>
-                    
+                    <TodoList  todoList={todoList}/>
+                                    
                 </div>    
             </div>
         )
