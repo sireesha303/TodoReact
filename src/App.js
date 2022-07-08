1148866208c1288ac86900fb10fab544af0040ff
sireesha <msireesha30031999@gmail.com>
2022-07-08 17:38:09 +0530

    chaged app component as functional component and sending refresh token with every one minute to get access token

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import './App.css';
-import { BrowserRouter,Route,Routes,Navigate } from "react-router-dom";
-
+import { Route,Routes,useNavigate } from "react-router-dom";
 import Login from './components/Login/index';
 import Home from './components/Home/index';
 import Signup from './components/Signup';
 import UserContext from './context/UserContext';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 import jwt_decode from "jwt-decode";
 
-class App extends Component{
-  state = {name:"",userId:"",isRefreshTokenExpaired:false}
+const  App = () =>{
+  const [contextData,setContextData] = useState({
+    name:"",
+    userId:""
+  });
 
-  
+  var navigate = useNavigate()  
 
-  setUserDetails = (name,userId)=>{
-    this.setState({name,userId})
+  const setUserDetails = (name,userId)=>{
+    setContextData({name,userId})
   }
 
-  logoutUser = () =>{
-    console.log("logout user called")
+  const logoutUser = () =>{
     Cookies.remove('todo-access-token')
     Cookies.remove('todo-refresh-token')
-    this.setUserDetails(null,null);
-    console.log("before navigate");
-    this.setState({isRefreshTokenExpaired:true})
-    console.log("after naviaget")
+    setUserDetails(null,null);
+    navigate("/login")
     
   }
 
-  updateTokens = async () =>{
+  const updateTokens = async () =>{
     const refreshToken  = Cookies.get('todo-refresh-token');
 
     const url = "http://127.0.0.1:8000/token/refresh/";
@@ -44,8 +43,6 @@ class App extends Component{
 
     const response = await fetch(url,options);
     const data = await response.json()
-    // console.log(response)
-    // console.log(data)
     if(response.status === 200){
       var accessToken = data.access;
       var inOneMin = new Date(new Date().getTime() + 1 * 60 * 1000)
@@ -53,32 +50,31 @@ class App extends Component{
       // var inFiveMinutes = new Date(new Date().getTime() + 5 * 60 * 1000);
       Cookies.set('todo-access-token',accessToken,{ expires: inOneMin });
       var decodedData = jwt_decode(accessToken);
-      this.setUserDetails(decodedData.name,decodedData.user_id);
+      setUserDetails(decodedData.name,decodedData.user_id);
     }
     else if(response.status === 401){
-      this.logoutUser()
+      logoutUser()
     }
   }
 
-  componentDidMount(){
-    this.updateTokens()
-  }
-
-  render(){
-    const {name,userId,isRefreshTokenExpaired} = this.state
-
+  useEffect(()=>{
+    const interval = setInterval(() => {
+      updateTokens();
+    }, 60000);
+    return () => clearInterval(interval);
+    
+  },[])
+  
     return(
-      <UserContext.Provider value={{name,userId, setUserDetails: this.setUserDetails}}> 
-      <BrowserRouter>
+      <UserContext.Provider value={{name:contextData.name,userId:contextData.userId, setUserDetails: setUserDetails}}> 
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Signup/>}/>
         </Routes>
-    </BrowserRouter>
-  </UserContext.Provider> 
+      </UserContext.Provider> 
     )
-  }
+  
 }
     
 
